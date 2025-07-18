04d129523ea5a529acc68fa077a259a68da2b6c6
Chris Corbett <ccorbett@amplience.com>
2025-07-18 10:53:52 +0100

    feat: make PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'earthmap8k.jpg'],
+      manifest: {
+        name: 'EdgeTX Log Viewer',
+        short_name: 'Log Viewer',
+        description: 'A 3D viewer for EdgeTX flight logs.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: '/edgetx-log-viewer/',
 })
